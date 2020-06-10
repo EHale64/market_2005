b6dd689fff868e9362e23295544220a03e3ce672
Eric Hale <eric_hale@comcast.net>
2020-06-10 09:24:06 -0600

    Add Vendor class and test suite

---
+++ test/vendor_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+      assert_instance_of Vendor, @vendor
+  end
+end
