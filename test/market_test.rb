b84dfcd415520583b69362adb4cba30df8a87e1e
Eric Hale <eric_hale@comcast.net>
2020-06-10 09:38:25 -0600

    Add Market and test suite

---
+++ test/market_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+      assert_instance_of Market, @market
+  end
+end
